2539f16ed4a1b50f11e784fcb2f11165957f886b
nandeepmannava <mannava.nandeep@gmail.com>
2020-05-08 11:59:26 -0500

    Updated cassandra backup bucket for staging

--- backup-incremental.sh
+++ backup-incremental.sh
@@ -1,1 +1,1 @@
-/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://losant_cassandra_backups -vi -N 10 >> /var/log/cassandra/backup-incremental.log 2>&1
+/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://staging_losant_backups/cassandra_backups -vi -N 10 >> /var/log/cassandra/backup-incremental.log 2>&1
