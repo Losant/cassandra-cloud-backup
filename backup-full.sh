d0ed88f43b6328bce98a83bc681f2c36bc381cb7
nandeepmannava <mannava.nandeep@gmail.com>
2020-10-01 12:39:09 -0500

    Fixed backup issue on centos machines

--- backup-full.sh
+++ backup-full.sh
@@ -1,1 +1,1 @@
-/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://losant_cassandra_backups -vCc -N 10 >> /var/log/cassandra/backup-full.log 2>&1
+/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://losant_cassandra_backups -vCc -N 10 -y /etc/cassandra/conf/cassandra.yaml >> /var/log/cassandra/backup-full.log 2>&1
--- backup-incremental.sh
+++ backup-incremental.sh
@@ -1,1 +1,1 @@
-/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://losant_cassandra_backups -vi -N 10 >> /var/log/cassandra/backup-incremental.log 2>&1
+/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://losant_cassandra_backups -vi -N 10 -y /etc/cassandra/conf/cassandra.yaml >> /var/log/cassandra/backup-incremental.log 2>&1
